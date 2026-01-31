028aee88b31f0e105c485e84cf159737d96742c8
Chahat <chahatbawa23@gmail.com>
2026-01-31 17:42:45 +0530

    Update email credentials in settings.py

--- TextTool/settings.py
+++ TextTool/settings.py
@@ -140,8 +140,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'tamanna20040923@gmail.com'
-EMAIL_HOST_PASSWORD = 'xvfn rkzh aygc kbjf'
+EMAIL_HOST_USER = 'your email-id'
+EMAIL_HOST_PASSWORD = 'your host password'
 
 
 
